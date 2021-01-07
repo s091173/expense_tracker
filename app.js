a5f934e115f834f20fffb5d8728807f4316f4a0b
Yuchi Su <astraiostor@gmail.com>
2021-01-07 17:31:33 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
+
+
 
 // 設定連線到 mongoDB
 mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -17,8 +20,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// setting template engine
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(3000, () => {
