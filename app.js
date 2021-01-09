61875e3d6684c8776c9169123b31785824f5966d
Yuchi Su <astraiostor@gmail.com>
2021-01-09 19:09:51 +0800

    feat: add delete record function

--- app.js
+++ app.js
@@ -102,6 +102,15 @@ app.post('/records/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// delete route 
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 app.listen(3000, () => {
   console.log('The app is running on http://localhost:3000')
