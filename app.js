f55d621a9abc465b3041c3c2f6510cda2f14fe68
Yuchi Su <astraiostor@gmail.com>
2021-03-20 19:34:04 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const handlebars = require('handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 // 引用路由器
 const routes = require('./routes')
 
@@ -32,9 +33,13 @@ app.use(bodyParser.urlencoded({ extended: true }), methodOverride('_method'))
 // 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
 
+app.use(flash())
+
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 
