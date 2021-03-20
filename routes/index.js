5c2767dd1fe54287dce6c299a0ccdfc0e778ca4e
Yuchi Su <astraiostor@gmail.com>
2021-03-20 13:50:34 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const router = express.Router()
 const home = require('./modules/home')
 // 引入 records 模組程式碼
 const records = require('./modules/records')
+// 引入 users 模組
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/records', records)
+router.use('/users', users)
 
 module.exports = router
